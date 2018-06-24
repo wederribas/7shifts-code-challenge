e3b2540ca53b64a540c4806fae18434cfcffd5bb
wederribas <wederribas@gmail.com>
2018-06-24 15:36:27 -0300

    Add API hostname to constants

--- api/config/constants.js
+++ api/config/constants.js
@@ -1,8 +1,10 @@
+const HOSTNAME = "http://127.0.0.1:8000";
 const LOCATIONS_ENDPOINT = `https://shiftstestapi.firebaseio.com/locations.json`;
 const USERS_ENDPOINT = `https://shiftstestapi.firebaseio.com/users.json`;
 const TIME_PUNCHES_ENDPOINT = `https://shiftstestapi.firebaseio.com/timePunches.json`;
 
 module.exports = {
+  HOSTNAME,
   LOCATIONS_ENDPOINT,
   USERS_ENDPOINT,
   TIME_PUNCHES_ENDPOINT
