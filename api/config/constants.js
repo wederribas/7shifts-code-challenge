a662820ad62fc8178ed794fdfc141175827e2ad0
wederribas <wederribas@gmail.com>
2018-06-24 12:22:30 -0300

    Fix constants module export with module.exports

--- api/config/constants.js
+++ api/config/constants.js
@@ -1,3 +1,9 @@
-export const LOCATIONS_ENDPOINT = `https://shiftstestapi.firebaseio.com/locations.json`;
-export const USERS_ENDPOINT = `https://shiftstestapi.firebaseio.com/users.json`;
-export const TIME_PUNCHES_ENDPOINT = `https://shiftstestapi.firebaseio.com/timePunches.json`;
+const LOCATIONS_ENDPOINT = `https://shiftstestapi.firebaseio.com/locations.json`;
+const USERS_ENDPOINT = `https://shiftstestapi.firebaseio.com/users.json`;
+const TIME_PUNCHES_ENDPOINT = `https://shiftstestapi.firebaseio.com/timePunches.json`;
+
+module.exports = {
+  LOCATIONS_ENDPOINT,
+  USERS_ENDPOINT,
+  TIME_PUNCHES_ENDPOINT
+};
