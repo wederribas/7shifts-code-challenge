16e0b190d8f56cf53821a828e2161d531cd2e846
wederribas <wederribas@gmail.com>
2018-06-24 15:36:59 -0300

    Removed SSL from server log

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const app = require("./config/express");
 const port = process.env.PORT || 8000;
 
 const server = app.listen(port, function() {
-  console.log("The server is running at https://127.0.0.1:" + port);
+  console.log("The server is running at http://127.0.0.1:" + port);
 });
 
 module.exports = app;
